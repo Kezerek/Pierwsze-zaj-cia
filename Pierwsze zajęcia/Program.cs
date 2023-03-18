51b058eb1a916bc4479c23e9a297ea14a450436f
Kezerek <pawel.nieradzik@gmail.com>
2023-03-18 13:39:05 +0100

    pierwsza modyfikacja

--- Pierwsze zajęcia/Program.cs
+++ Pierwsze zajęcia/Program.cs
@@ -1,2 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System;
+
 Console.WriteLine("To mój pierwszy program przesłany na githuba");
+Console.WriteLine("Pierwsza Modyfikacja");
